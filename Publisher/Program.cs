628e5f57f758f9a812027789d96d80130149b6d1
ahmetcanakdas <acanakdas@gmail.com>
2022-01-20 02:50:37 +0300

    first commit

---
+++ Publisher/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            FanoutExchange();
+
+
+
+        }
+
+
+        static void FanoutExchange()
+        {
+            //FANOUT EXCHANGE//
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Log {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
+                Console.WriteLine($"Log {x} Sent");
+            });
+
+            Console.ReadLine();
+        }
+
+
+
+        private static void ToTheQueue()
+        {
+            //Directly to the queue// 
+
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            //durable: false ise oluşan kuyruklar memory'de tutulur, rabbit mq restart olduğunda kuyruklar silinir
+            //true ise fiziksel olarak tutulur
+            //exclusive: false ise başka kuyruklardan bağlanılabilir
+            channel.QueueDeclare("hello-queue", durable: true, exclusive: false, autoDelete: false);
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+                Console.WriteLine($"Message {x} Sent");
+            });
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
---
+++ Subscriber/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getFromFanoutExchange();
+        }
+
+
+
+        private static void getFromFanoutExchange()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var randomQueueName = channel.QueueDeclare().QueueName;
+
+
+            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(randomQueueName, false, consumer);
+
+            Console.WriteLine($"Listening queue: {randomQueueName}...");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                Thread.Sleep(100);
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj:" + message);
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+            Console.ReadLine();
+        }
+
+
+
+        private static void getFromQueue()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, false);
+
+
+            //durable: false ise oluşan kuyruklar memory'de tutulur, rabbit mq restart olduğunda kuyruklar silinir
+            //true ise fiziksel olarak tutulur
+            //exclusive: false ise başka kuyruklardan bağlanılabilir
+            //channel.QueueDeclare("hello-queue", durable: true, exclusive: false, autoDelete: false);
+
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("hello-queue", false, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                Thread.Sleep(100);
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj:" + message);
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+            Console.ReadLine();
+        }
+    }
+}
